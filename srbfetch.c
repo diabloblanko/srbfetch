4a9e028ed15961177ffd0fd5292569f7fdebfecb
diabloblanco <diabloblanco@noreply.codeberg.org>
2025-06-03 11:53:53 +0200

    Added NetBSD suppport

--- srbfetch.c
+++ srbfetch.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "./vars.h"
-
-#define VERSION "1.3"
+#include <string.h>
+// 15
+#define VERSION "1.3.1"
 #define RESET "\033[0m"
 #define BLACK "\033[30m" 
 #define YELLOW "\033[33m" 
@@ -13,7 +14,7 @@
 #define BGBL "\x1b[44m" // Blue Background
 #define SEPARATOR "= "
 //#define OSNAME "XIU" //XNU MENTIONED // X Is UNIX
-
+const char osname[] = get_kernel;
 
 void srb_linux(void) {
         printf(
@@ -28,6 +29,29 @@ void srb_linux(void) {
                 BGWH WHITE "#######################" RESET "\n" 
         );      
                  }
+void srb_unknown() {
+        printf(
+                        RED " ## " RESET RED "S" RESET BLUE "R" RESET WHITE "B" RESET "Fetch" VERSION "\n"
+                        RED "#  #" RESET "OS " SEPARATOR get_kernel "\n"
+                        BLUE "  # " RESET "Kernel " SEPARATOR get_kernel " " get_kernel_ver "\n"
+                        BLUE "  # " RESET "Shell " SEPARATOR get_shell "\n"
+                        "    Hostname " SEPARATOR get_hostname "\n"
+                        WHITE "  # " RESET "\n"
+            );
+}
+void srb_openbsd();
+void srb_netbsd() {
+      printf(
+          "*" BGRD RED "###### " RESET RED " S" RESET BLUE "R" RESET WHITE "B" RESET "Fetch " VERSION "\n"
+          "*" BGBL BLUE "###### " RESET " OS " SEPARATOR "SRB_BSD\n"
+          "*" BGWH WHITE "###### " RESET " Kernel " SEPARATOR get_kernel " " get_kernel_ver "\n"
+          "*" "        Shell " SEPARATOR get_shell "\n"
+          "*" "        Hostname " SEPARATOR get_hostname "\n" ); }
+void srb_freebsd();
 int main() {
-	srb_linux();
+	if(strcmp(osname, "Linux")   == 0)   { srb_linux();   }
+  else if(strcmp(osname, "OpenBSD") == 0)   { srb_openbsd(); }
+  else if(strcmp(osname, "NetBSD")  == 0)   { srb_netbsd();  }
+  else if(strcmp(osname, "FreeBSD") == 0)   { srb_freebsd(); }
+  else { srb_unknown(); }
 }
